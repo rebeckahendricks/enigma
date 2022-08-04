e76d948387a5d257973914048d95dd6a2af1d3c2
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-04 10:36:14 -0600

    Test: Flesh out enigma test structure

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,4 +9,47 @@ describe Enigma do
   it 'exists' do
     expect(@enigma).to be_instance_of(Enigma)
   end
+
+  xit 'can encrypt a message with a key and date' do
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      }
+
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
+  xit 'can decrypt a message with a key and date' do
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+      }
+
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
+  end
+#~~~~incomplete specs below~~~~#
+  xit 'can encrypt a message with a key (uses todays date)' do
+    encrypted = {
+      encryption: ,
+      key: ,
+      date:
+      }
+      #probable mocks & stubs here
+    expect(@enigma.encrypt("hello world", "02715")).to eq(encrypted)
+  end
+
+  xit 'can decrypt a message with a key (uses todays date)' do
+    encrypted = @enigma.encrypt("hello world", "02715")
+    decrypted = {}
+
+    expect(@enigma.decrypt(encrypted[:encryption], "02715")).to eq(decrypted)
+  end
+
+  xit 'can encrypt a message (generates random key and uses todays date)' do
+    encryted = {}
+    #definite mocks & stubs here
+    expect(@enigma.encrypt("hello world")).to eq(encrypted)
+  end
 end
